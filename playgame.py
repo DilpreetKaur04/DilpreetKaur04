80510b8731d5a509410bafaf83f6efa1ef6ce00f
Dilpreet Kaur <dilpreetkaur_04@yahoo.com>
2026-02-03 10:38:58 +0000

    Created using Colab

---
+++ playgame.py
@@ -0,0 +1,28 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMZDHynvyn+1e1JjDNu+pC9"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "DJ90tqRXEmtL"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
